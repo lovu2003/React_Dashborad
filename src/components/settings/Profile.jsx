00547a6f981a6ec08951559c6ef59188f4706783
Lovely  Kumari <lovely03kumari@gmail.com>
2024-09-30 17:07:08 +0530

    first

---
+++ src/components/settings/Profile.jsx
@@ -0,0 +1,28 @@
+import { User } from "lucide-react";
+import SettingSection from "./SettingSection";
+
+const Profile = () => {
+  return (
+    <SettingSection icon={User} title={"Profile"}>
+      <div className="flex flex-col sm:flex-row items-center mb-6">
+        <img
+          src="https://raw.githubusercontent.com/lovu2003/Photo/main/photolk.jpg
+
+"
+          alt="Profile"
+          className="rounded-full w-20 h-20 object-cover mr-4"
+        />
+
+        <div>
+          <h3 className="text-lg font-semibold text-gray-100">Lovely Kumari</h3>
+          <p className="text-gray-400">vatsya@gmail.com</p>
+        </div>
+      </div>
+
+      <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
+        Edit Profile
+      </button>
+    </SettingSection>
+  );
+};
+export default Profile;
